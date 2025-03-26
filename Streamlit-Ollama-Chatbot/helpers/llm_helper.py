582438197445a40800edc3bd5ac46ab4f1876976
Clouddude <klouddude@gmail.com>
2025-03-26 11:48:22 -0400

    Update llm_helper.py
    add base_url

--- Streamlit-Ollama-Chatbot/helpers/llm_helper.py
+++ Streamlit-Ollama-Chatbot/helpers/llm_helper.py
@@ -9,6 +9,7 @@ system_prompt = Config.SYSTEM_PROMPT
 
 def chat(user_prompt, model):
     stream = ollama.chat(
+        base_url="http://40.233.69.157:11434",  # specify your custom URL here
         model=model,
         messages=[{'role': 'assistant', 'content': system_prompt},
                   {'role': 'user', 'content': f"Model being used is {model}.{user_prompt}"}],
