432db5400ef4ce1fa525c7da8d18469f0e2ce94c
Clouddude <klouddude@gmail.com>
2025-03-27 19:19:07 -0400

    Update config.py

--- Streamlit-Ollama-Chatbot/config.py
+++ Streamlit-Ollama-Chatbot/config.py
@@ -4,8 +4,8 @@ Demo
 
 class Config:
     PAGE_TITLE = "Funky Ollama3 Chatbot"
-
-    OLLAMA_MODELS = ('llama3', 'codellama:13b') 
+    BASE_URL = "http://1270.0.0.1"   # Use Your remote ollama base URL if not local or the port != 11443  
+    OLLAMA_MODELS = ('llama3', 'codellama:13b') # CHANGE ME - it has to match available model in your ollama backend
     # , 'llama2-uncensored' ,'llama2:7b', 'llama2:13b', 'mistral', 'mixtral')
 
     SYSTEM_PROMPT = f"""You are a helpful chatbot that has access to the following 
