78113fc4a6dcb5bd8c1d0f193f92474004ecf4cd
Clouddude <klouddude@gmail.com>
2025-03-27 19:14:55 -0400

    Update llm_helper.py
    add custom URL endpoint

--- Streamlit-Ollama-Chatbot/helpers/llm_helper.py
+++ Streamlit-Ollama-Chatbot/helpers/llm_helper.py
@@ -4,18 +4,20 @@ Demo
 
 import ollama
 from config import Config
-
+# sets up the system prompt
 system_prompt = Config.SYSTEM_PROMPT
 
 def chat(user_prompt, model):
-    stream = ollama.chat(
-      #  base_url="http://40.233.69.157:11434",  # specify your custom URL here
+    # Instantiate the client with the custom URL
+    client = ollama.Client(host=Config.BASE_URL)
+    # Use the client to initiate the chat with the model
+    stream = client.chat(
         model=model,
         messages=[{'role': 'assistant', 'content': system_prompt},
                   {'role': 'user', 'content': f"Model being used is {model}.{user_prompt}"}],
         stream=True,
     )
-
+   # print(stream)
     return stream
 
 # handles stream response back from LLM
